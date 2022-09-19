fcee6d9383b4b178886a2d2145135438bfa2df9e
artem-from-spb <96229835+artem-from-spb@users.noreply.github.com>
2022-09-19 08:06:04 +0300

    structure ok

--- app.js
+++ app.js
@@ -5,13 +5,17 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
-// подключаемся к серверу mongo
-mongoose.connect("mongodb://localhost:27017/mestodb", {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
+app.use("/users", (req, res) => {
+  res.send(`<h1>Мы на ${PORT} порту</h1>`)
 });
 
-app.listen(PORT, (req, res) => {
-  console.log(req, res);
+// подключаемся к серверу mongo
+// mongoose.connect("mongodb://localhost:27017/mestodb", {
+//   useNewUrlParser: true,
+//   useCreateIndex: true,
+//   useFindAndModify: false,
+// });
+
+app.listen(PORT, () => {
+  console.log(PORT);
 });
--- models/user.js
+++ models/user.js
@@ -18,3 +18,5 @@ const userSchema = new mongoose.Schema({
     required: true,
   }
 })
+
+module.exports = mongoose.model('user', userSchema);
