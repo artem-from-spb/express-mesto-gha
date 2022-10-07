80615c16f41e460a3f89a5ac63ee6e08058441ce
Artem Ryzhov <artem-from-spb@yandex.ru>
2022-10-07 10:51:01 +0300

    celebrate added3

--- app.js
+++ app.js
@@ -31,7 +31,9 @@ app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
     about: Joi.string().required().min(2).max(30),
-    avatar: Joi.string().pattern(),
+    avatar: Joi.string().pattern(
+      /https?:\/\/(www\.)?[a-zA-Z\d\-.]{1,}\.[a-z]{1,6}([/a-z0-9\-._~:?#[\]@!$&'()*+,;=]*)/
+    ),
     email: Joi.string().required(),
     password: Joi.string().required(),
   })
