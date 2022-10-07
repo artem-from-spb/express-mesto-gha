315fd1dd1d1693fdb925f20e298ba3b46826e837
Artem Ryzhov <artem-from-spb@yandex.ru>
2022-10-07 20:17:33 +0300

    validation update20

--- app.js
+++ app.js
@@ -38,6 +38,13 @@ mongoose.connect("mongodb://localhost:27017/mestodb");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required(),
+    password: Joi.string().required(),
+  })
+}), login);
+
 app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
@@ -50,20 +57,15 @@ app.post('/signup', celebrate({
   })
 }), createUser);
 
-app.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required(),
-    password: Joi.string().required(),
-  })
-}), login);
+
 
 app.use(auth);
-app.use(errors());
+
 
 app.use("/users", routerUsers);
 app.use("/cards", routerCards);
 
-
+app.use(errors());
 
 
 app.use(() => {
