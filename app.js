dfd4bbea969100460122da5a4e7a78fd8d4a8693
Artem Ryzhov <artem-from-spb@yandex.ru>
2022-10-07 10:32:43 +0300

    celebrate added

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const { celebrate, Joi, errors } = require("celebrate");
+
 const routerUsers = require("./routes/users");
 const routerCards = require("./routes/cards");
 const auth = require("./middlewares/auth");
-
 const { createUser, login } = require("./controllers/users");
+const NotFoundErrorStatus = require("./errors/NotFoundError");
 
 const { PORT = 3000 } = process.env;
 
@@ -16,17 +18,47 @@ const app = express();
 mongoose.connect("mongodb://localhost:27017/mestodb");
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/signin', login);
-app.post('/signup', createUser);
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required(),
+    password: Joi.string().required(),
+  })
+}), login);
+
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2).max(30),
+    avatar: Joi.string().pattern(),
+    email: Joi.string().required(),
+    password: Joi.string().required(),
+  })
+}), createUser);
 
 app.use(auth);
+app.use(errors());
 
 app.use("/users", routerUsers);
 app.use("/cards", routerCards);
 
 app.use("*", (req, res) => {
-  res.status(404).send({ message: "Ошибка 404" });
+  res.status(NotFoundErrorStatus).send({ message: "Ошибка 404" });
+});
+
+app.use((err, req, res, next) => {
+  // если у ошибки нет статуса, выставляем 500
+  const { statusCode = 500, message } = err;
+
+  res
+    .status(statusCode)
+    .send({
+      // проверяем статус и выставляем сообщение в зависимости от него
+      message: statusCode === 500
+        ? 'На сервере произошла ошибка'
+        : message
+    });
 });
 
 app.listen(PORT, () => console.log(PORT));
