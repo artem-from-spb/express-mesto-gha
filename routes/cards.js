7472118f2c2639756f344cb959df5470fdadbc61
Artem Ryzhov <artem-from-spb@yandex.ru>
2022-10-09 14:31:12 +0300

    create prob8

--- routes/cards.js
+++ routes/cards.js
@@ -1,4 +1,6 @@
 const router = require("express").Router();
+const { celebrate, Joi } = require('celebrate');
+
 const {
   createCard,
   getCards,
@@ -8,9 +10,48 @@ const {
 } = require("../controllers/cards");
 
 router.get("/", getCards);
-router.post("/", createCard);
-router.delete("/:cardId", deleteCard);
-router.put("/:cardId/likes", setLike);
-router.delete("/:cardId/likes", removeLike);
+
+router.post(
+  '/',
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required().min(2).max(30),
+      link: Joi.string()
+        .required()
+        .pattern(/https?:\/\/(www\.)?[a-zA-Z\d\-.]{1,}\.[a-z]{1,6}([/a-z0-9\-._~:?#[\]@!$&'()*+,;=]*)/),
+    }),
+  }),
+  createCard,
+);
+
+router.delete(
+  '/:cardId',
+  celebrate({
+    params: Joi.object().keys({
+      cardId: Joi.string().length(24).hex(),
+    }),
+  }),
+  deleteCard,
+);
+
+router.put(
+  '/:cardId/likes',
+  celebrate({
+    params: Joi.object().keys({
+      cardId: Joi.string().length(24).hex(),
+    }),
+  }),
+  setLike,
+);
+
+router.delete(
+  '/:cardId/likes',
+  celebrate({
+    params: Joi.object().keys({
+      cardId: Joi.string().length(24).hex(),
+    }),
+  }),
+  removeLike,
+);
 
 module.exports = router;
